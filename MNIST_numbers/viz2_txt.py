a91a6f87e2793dd7aa2e7c98eb2a15e8b4f74ed9
Linnea <lm686@cornell.edu>
2018-10-19 16:19:15 -0400

    reorganized and added file for training on MNIST clothes

---
+++ MNIST_numbers/viz2_txt.py
@@ -0,0 +1,149 @@
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import csv
+import numpy as np
+mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+
+# Python optimisation variables
+learning_rate = 0.00146
+epochs = 10
+batch_size = 25
+
+# declare the training data placeholders
+# input x - for 28 x 28 pixels = 784 - this is the flattened image data that is drawn from
+# mnist.train.nextbatch()
+x = tf.placeholder(tf.float32, [None, 784])
+# dynamically reshape the input
+x_shaped = tf.reshape(x, [-1, 28, 28, 1])
+# now declare the output data placeholder - 10 digits
+y = tf.placeholder(tf.float32, [None, 10])
+
+
+def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_shape, pool_shape, name):
+    # setup the filter input shape for tf.nn.conv_2d
+    conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels,
+                       num_filters]
+
+    # initialise weights and bias for the filter
+    weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03),
+                          name=name+'_W')
+    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name+'_b')
+
+    # setup the convolutional layer operation
+    out_layer = tf.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='SAME')
+
+    # add the bias
+    out_layer += bias
+
+    # apply a ReLU non-linear activation
+    out_layer = tf.nn.relu(out_layer)
+
+    # now perform max pooling
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, 2, 2, 1]
+    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides,
+                               padding='SAME')
+
+    return out_layer
+
+
+# create some convolutional layers
+layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
+layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
+
+flattened = tf.reshape(layer2, [-1, 7 * 7 * 64])
+
+# setup some weights and bias values for this layer, then activate with ReLU
+wd1 = tf.Variable(tf.truncated_normal(
+    [7 * 7 * 64, 1000], stddev=0.03), name='wd1')
+bd1 = tf.Variable(tf.truncated_normal([1000], stddev=0.01), name='bd1')
+dense_layer1 = tf.matmul(flattened, wd1) + bd1
+dense_layer1 = tf.nn.relu(dense_layer1)
+
+# another layer with softmax activations
+wd2 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.03), name='wd2')
+bd2 = tf.Variable(tf.truncated_normal([10], stddev=0.01), name='bd2')
+dense_layer2 = tf.matmul(dense_layer1, wd2) + bd2
+y_ = tf.nn.softmax(dense_layer2)
+
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(logits=dense_layer2, labels=y))
+
+# add an optimiser
+optimiser = tf.train.AdamOptimizer(
+    learning_rate=learning_rate).minimize(cross_entropy)
+
+# define an accuracy assessment operation
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# setup the initialisation operator
+init_op = tf.global_variables_initializer()
+
+depth = 10
+finalReps = []
+
+saver = tf.train.Saver()
+
+with open("weights_1_multepochs.txt", 'w') as file:
+    # file.write("test")
+    with tf.Session() as sess:
+        # initialise the variables
+        print("starting session")
+        sess.run(init_op)
+        total_batch = int(len(mnist.train.labels) / batch_size)
+        for epoch in range(epochs):
+            print("starting epcoh:", (epoch+1))
+            avg_cost = 0
+            for i in range(total_batch):
+                # print("batch ", (i+1))
+                if (i % 100 == 0):
+                    print("batch ", i)
+                batch_x, batch_y = mnist.train.next_batch(
+                    batch_size=batch_size)
+                indices = batch_y
+                y_matrix = tf.one_hot(indices, depth).eval(session=sess)
+                # print("encoding scores")
+                _, c = sess.run([optimiser, cross_entropy],
+                                feed_dict={x: batch_x, y: y_matrix})
+                avg_cost += c / total_batch
+            y_test_matrix = tf.one_hot(
+                mnist.test.labels[:100], depth).eval(session=sess)
+            test_acc = sess.run(accuracy,
+                                feed_dict={x: mnist.test.images[:100], y: y_test_matrix})
+            print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost),
+                  "test accuracy: {: .3f}".format(test_acc))
+
+        file.write("epoch"+str(i)+"\n")
+        file.write("layer1\n")
+        weight = dense_layer1.eval(session=sess, feed_dict={
+            x: mnist.test.images[:500]})
+        # finalReps.append(weight)
+        print("weights at epoch: ", weight)
+        weights_str = list(
+            map(lambda x: np.array2string(np.array(x), separator=","), weight))
+        file.write(np.array2string(np.array(weights_str), separator=","))
+        # save_path = saver.save(sess, "/tmp/model2.ckpt")
+        file.write('\n')
+
+        # saver.restore(sess, "/tmp/model2.ckpt")
+        file.write("layer2\n")
+        weight = dense_layer2.eval(session=sess, feed_dict={
+            x: mnist.test.images[:500]})
+        # finalReps.append(weight)
+        print("weights at epoch: ", weight)
+        weights_str = list(
+            map(lambda x: np.array2string(np.array(x), separator=","), weight))
+        file.write(np.array2string(np.array(weights_str), separator=","))
+        save_path = saver.save(sess, "/tmp/model2.ckpt")
+        file.write('\n')
+
+
+print("\nTraining complete!")
+print(sess.run(accuracy, feed_dict={
+    x: mnist.test.images, y: mnist.test.labels}))
+
+f = open("backup.txt")
+f.write(array2string(finalReps))
--- README.md
+++
@@ -1,2 +0,0 @@
-# insights-vizNNs
-Insights project on building tools to visualize the evolution of advanced neural networks. 
