c7b768bc8d00a1a5d23172c536fad6e353705b63
Linnea <lm686@cornell.edu>
2018-12-03 13:42:27 -0500

    path data for viz

--- viz/pointPath.js
+++ viz/pointPath.js
@@ -1,10 +1,10 @@
 var margin = {
   top: 20,
-  right: 20,
+  right: 50,
   bottom: 50,
-  left: 20
+  left: 50
 },
-  width = 900 - margin.left - margin.right,
+  width = 1000 - margin.left - margin.right,
   height = 900 - margin.top - margin.bottom;
 
 var canvas = d3.select("body").append("svg").attr('width', width + margin.left + margin.right)
@@ -13,8 +13,31 @@ var canvas = d3.select("body").append("svg").attr('width', width + margin.left +
   .append('g')
   .attr('transform', `translate(${margin.left},${margin.top})`);
 
-function colorLuminance(hex, lum) {
+var defs = canvas.append("defs");
 
+function setGradient(class_num, start_color, stop_color) {
+  var gradient = defs.append("linearGradient")
+    .attr("id", "svgGradient" + class_num)
+    .attr("x1", "0%")
+    .attr("x2", "100%")
+    .attr("y1", "0%")
+    .attr("y2", "100%");
+
+  gradient.append("stop")
+    .attr('class', 'start')
+    .attr("offset", "0%")
+    .attr("stop-color", start_color)
+    .attr("stop-opacity", 1);
+
+  gradient.append("stop")
+    .attr('class', 'end')
+    .attr("offset", "100%")
+    .attr("stop-color", stop_color)
+    .attr("stop-opacity", 1);
+}
+
+
+function colorLuminance(hex, lum) {
   // validate hex string
   hex = String(hex).replace(/[^0-9a-f]/gi, '');
   if (hex.length < 6) {
@@ -56,6 +79,8 @@ function plot_epoch(data, epoch_num, saturation_scale, x, y, color, circle) {
   return circle;
 }
 
+
+
 d3.json('layer2_layout.json', function (data) {
   console.log(data);
   //var e0 = data["points"][0]["epoch0"];
@@ -64,11 +89,11 @@ d3.json('layer2_layout.json', function (data) {
   //console.log(e0_y);
 
   var x = d3.scaleLinear()
-    .range([0, width])
+    .range([margin.left, width])
     .domain(d3.extent(e0_x));
 
   var y = d3.scaleLinear()
-    .range([height, 0])
+    .range([height, margin.bottom])
     .domain([d3.max(e0_y), d3.min(e0_y)]);
 
   var xAxis = d3.axisBottom(x);
@@ -81,6 +106,58 @@ d3.json('layer2_layout.json', function (data) {
   plot_epoch(data["points"], 3, 0, x, y, color);
   plot_epoch(data["points"], 4, 0.2, x, y, color);
 
+  /* var lines = canvas.selectAll("line")
+    .data(data["points"])
+    .enter()
+    .append("line")
+    .attr("x1", function (d) {
+      //console.log(x(d["epoch0"][0]));
+      return x(d["epoch0"][0]);
+    })
+    .attr("x2", function (d) {
+      //console.log(x(d["epoch1"][0]));
+      return x(d["epoch1"][0]);
+    })
+    .attr("y1", function (d) {
+      //console.log(y(d["epoch0"][1]));
+      return y(d["epoch0"][1]);
+    })
+    .attr("y2", function (d) {
+      //console.log(y(d["epoch1"][1]));
+      return y(d["epoch1"][1]);
+    })
+    .attr("stroke", "grey")
+    .attr('stroke-width', '1px')
+    .attr('opacity', 0.7); */
+
+  var line = d3.line()
+    .x(function (d) { return x(d[0]) })
+    .y(function (d) { return y(d[1]) })
+    .curve(d3.curveBundle);
+
+  var point_0 = [
+    data["points"][0]["epoch0"],
+    data["points"][0]["epoch1"],
+    data["points"][0]["epoch2"],
+    data["points"][0]["epoch3"],
+    data["points"][0]["epoch4"]
+  ];
+
+  //console.log(point_0);
+  var pathdata = line(point_0);
+  //console.log(pathdata);
+
+  //set gradient for lines with label i
+  for (var i = 0; i < 10; i++) {
+    setGradient(i, colorLuminance(color(i), -0.8), colorLuminance(color(i), 0.4));
+  }
+  //setGradient(9, colorLuminance(color(0), -0.6), colorLuminance(color(0), 0.2));
+
+  canvas.append("path")
+    .attr('d', pathdata)
+    .attr("stroke", "url(#svgGradient9)")
+    .attr("stroke-width", 2)
+    .attr("fill", "none");
 });
 
 
